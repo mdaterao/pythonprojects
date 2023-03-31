ff4a3887cd1ee097c94d0aeb732d1a80313e6d46
mdaterao <67026474+mdaterao@users.noreply.github.com>
2023-03-30 21:25:41 -0400

    Add files via upload

--- vec2d.py
+++ vec2d.py
@@ -26,12 +26,15 @@ class Point:
     pass
     
 class Vec2D(Point):
-    def __init__(self,obj1 = Point(), obj2 = Point()):
-        Point.__init__(self, x=0, y=0)
-        num1= obj1
-        num2 = obj2
-        self.x = num2.x - num1.x
-        self.y = num2.y - num1.y
+    def __init__(self,x=0,y=0):
+        if isinstance(x,int) and isinstance(y,int):
+            self.x = x
+            self.y = y
+        else:
+            num1 = Point(x)
+            num2 = Point(y)
+            self.x = int(num2.x) - int(num1.x)
+            self.y = int(num2.y) - int(num1.y)
         
     pass
 
